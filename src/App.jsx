18e231cb2db25612b5ee80bf6278733c91e3573f
codewithshahan <programmingwithshahan@gmail.com>
2021-12-25 20:21:17 +0600

    Chapter 6: Navbar and Routing Solution

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import Movies from "./components/Movies";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Customers from "./components/Customers";
+import Rentals from "./components/Rentals";
+import MovieForm from "./components/MovieForm";
+import NotFound from "./components/NotFound";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Switch>
+        <Route path="/movies/:id" component={MovieForm} />
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/movieform" component={MovieForm}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+        <Redirect from="/" exact to="/movies" />
+        <Redirect to="/not-found" />
+      </Switch>
+    </React.Fragment>
+  );
+}
+
+export default App;
--- src/components/MoviesTable.jsx
+++ src/components/MoviesTable.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Like from "./common/like";
 import Table from "./common/table";
 
@@ -6,7 +7,13 @@ function MoviesTable(props) {
   const { movies, onLike, OnDelete, onSort, sortColumn } = props;
 
   const columns = [
-    { path: "title", label: "Title" },
+    {
+      path: "title",
+      label: "Title",
+      content: (movie) => (
+        <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+      ),
+    },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
